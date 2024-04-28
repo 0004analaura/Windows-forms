97cfc8df690ab30532409ac3a7f6c2492730d0fb
Ana Laura Oliva <aolivac4@miumg.edu.gt>
2024-04-27 22:30:34 -0600

    Desafios de windows forms

---
+++ Desafio1forms/Desafio1forms/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace Desafio1forms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.CapitalInicial = new System.Windows.Forms.TextBox();
+            this.TasaInteres = new System.Windows.Forms.TextBox();
+            this.Tiempo = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Resultado = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.Calcular = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS Reference Sans Serif", 19.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(291, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(239, 42);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Capital Final";
+            // 
+            // CapitalInicial
+            // 
+            this.CapitalInicial.BackColor = System.Drawing.Color.White;
+            this.CapitalInicial.Location = new System.Drawing.Point(262, 100);
+            this.CapitalInicial.Name = "CapitalInicial";
+            this.CapitalInicial.Size = new System.Drawing.Size(295, 22);
+            this.CapitalInicial.TabIndex = 1;
+            // 
+            // TasaInteres
+            // 
+            this.TasaInteres.Location = new System.Drawing.Point(262, 167);
+            this.TasaInteres.Name = "TasaInteres";
+            this.TasaInteres.Size = new System.Drawing.Size(295, 22);
+            this.TasaInteres.TabIndex = 2;
+            // 
+            // Tiempo
+            // 
+            this.Tiempo.Location = new System.Drawing.Point(262, 235);
+            this.Tiempo.Name = "Tiempo";
+            this.Tiempo.Size = new System.Drawing.Size(295, 22);
+            this.Tiempo.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Tai Le", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(258, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 22);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Capital Inicial";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Tai Le", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(270, 139);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(129, 22);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Tasa de interes ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Tai Le", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(270, 205);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 22);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Tiempo";
+            // 
+            // Resultado
+            // 
+            this.Resultado.Enabled = false;
+            this.Resultado.Location = new System.Drawing.Point(262, 404);
+            this.Resultado.Name = "Resultado";
+            this.Resultado.Size = new System.Drawing.Size(295, 22);
+            this.Resultado.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Tai Le", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(270, 379);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(85, 22);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Resultado";
+            // 
+            // Calcular
+            // 
+            this.Calcular.Font = new System.Drawing.Font("Microsoft YaHei", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Calcular.Location = new System.Drawing.Point(307, 282);
+            this.Calcular.Name = "Calcular";
+            this.Calcular.Size = new System.Drawing.Size(183, 65);
+            this.Calcular.TabIndex = 9;
+            this.Calcular.Text = "Calcular";
+            this.Calcular.UseVisualStyleBackColor = true;
+            this.Calcular.Click += new System.EventHandler(this.Calcular_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Calcular);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.Resultado);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Tiempo);
+            this.Controls.Add(this.TasaInteres);
+            this.Controls.Add(this.CapitalInicial);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Calculadora";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox CapitalInicial;
+        private System.Windows.Forms.TextBox TasaInteres;
+        private System.Windows.Forms.TextBox Tiempo;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox Resultado;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button Calcular;
+    }
+}
+
---
+++ Desafio1forms/Desafio1forms/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Desafio1forms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Calcular_Click(object sender, EventArgs e)
+        {
+            double capitalInicialtex = double.Parse(CapitalInicial.Text);
+            double TasadeInterestext = double.Parse(TasaInteres.Text);
+            double Periodtext = double.Parse(Tiempo.Text);
+
+           
+
+            double resultado = capitalInicialtex *( Math.Pow(1 + TasadeInterestext, Periodtext));
+            resultado = Math.Round(resultado, 2);
+
+            Resultado.Text = resultado.ToString();
+
+        }
+    }
+}
